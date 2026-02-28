329fdf4106fdbdb4b35e9a598d703624f97e7351
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-02-28 12:35:36 -0400

    Getting the value of dialog box

--- src/ui.js
+++ src/ui.js
@@ -2,11 +2,22 @@
 
 const newTask = document.querySelector(".newTaskButton");
 const newTaskDialog = document.getElementById("newTaskDialog");
+const confirmButton = document.getElementById("confirmButton");
+const outputBox = document.querySelector("output");
 
 newTask.addEventListener("click", () => {
     newTaskDialog.showModal();
 });
 
+confirmButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    newTaskDialog.close();
+});
+
+let taskTitle = document.querySelector("#taskTitle").value;
+let taskDescription = document.querySelector("#taskDescription").value;
+let taskPriority = document.querySelector("#taskDueDate").value;
+let taskDueDate = document.querySelector("#taskPriority").value;
 
 
 
